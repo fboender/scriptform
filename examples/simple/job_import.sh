44e7d69444accdd75f0568ef4a58a6ecb9094b6c
Ferry Boender <ferry.boender@gmail.com>
2015-03-30 21:33:56 +0200

    Updates to the 'simple' example.

--- examples/simple/job_import.sh
+++ examples/simple/job_import.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+
 MYSQL_DEFAULTS_FILE="my.cnf"
 MYSQL="mysql --defaults-file=$MYSQL_DEFAULTS_FILE"
 
-echo "echo 'DROP DATABASE scriptform_acc' | $MYSQL"
-echo "$MYSQL < dbs/${sample_db}.sql"
+echo "This is what would be executed if this wasn't a fake script:"
+echo 
+echo "echo 'DROP DATABASE $target_db' | $MYSQL"
+echo "$MYSQL ${target_db} < ${sql_file}"
