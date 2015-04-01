dfe9cc2393d4be17bdc144f7c7ed963fe663c0b3
Ferry Boender <ferry.boender@gmail.com>
2015-04-01 19:22:36 +0200

    Simple example: rename htaccess to htpasswd.

--- examples/simple/job_add_user.sh
+++ examples/simple/job_add_user.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-HTACCESS=htaccess
+HTPASSWD=htpasswd
 
 err() {
     echo $* >&2
     exit 1
 }
+
+echo $username
+echo $password1
+echo $password2
 
 if [ -z "$password1" ]; then
     err "Empty password specified"
@@ -14,13 +18,13 @@ if [ "$password1" != "$password2" ]; then
     err "Passwords do not match."
 fi
 
-if [ $(egrep "^$username:" $HTACCESS) ]; then
+if [ $(egrep "^$username:" $HTPASSWD) ]; then
     UPDATE=1
 else
     UPDATE=0
 fi
 
-htpasswd -s -b $HTACCESS $username $password1 || exit $?
+htpasswd -s -b $HTPASSWD $username $password1 || exit $?
 
 if [ "$UPDATE" -eq 1 ]; then
     echo "User password updated"
