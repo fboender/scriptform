5183c6386a94d1a97484e4701813afff20bbbc9d
Ferry Boender <ferry.boender@electricmonk.nl>
2017-01-04 12:45:01 +0100

    Removed quotes around content disposition header in examples

--- examples/megacorp_acc/job_download_db.sh
+++ examples/megacorp_acc/job_download_db.sh
@@ -4,7 +4,7 @@ FILESIZE=$(stat -c "%s" megacorp.db)
 cat << EOF
 HTTP/1.0 200 Ok
 Content-Type: application/octet-stream
-Content-Disposition: attachment; filename="megacorp.db"
+Content-Disposition: attachment; filename=megacorp.db
 Content-Length: $FILESIZE
 
 EOF
--- examples/output_types/job_large_bin.sh
+++ examples/output_types/job_large_bin.sh
@@ -4,7 +4,7 @@ FILESIZE=$(expr 1024 \* 1000 \* 100)
 cat << EOF
 HTTP/1.0 200 Ok
 Content-Type: application/octet-stream
-Content-Disposition: attachment; filename="large_file.dat"
+Content-Disposition: attachment; filename=large_file.dat
 Content-Length: $FILESIZE
 
 EOF
