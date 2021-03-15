a726b577b6fc8062b7bc6b128fd9db026ca4af3b
Ferry Boender <ferry.boender@gmail.com>
2021-03-15 11:51:51 +0100

    Clarify test.sh script purpose

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+#
+# Script that's called by formconfig to test things. This is not the main
+# script to run tests. For that, see the "build.sla" file in the root dir of
+# this project.
+#
+
 cat << EOF
 HTTP/1.0 200 Ok
 echo "Content-type: text/plain"
